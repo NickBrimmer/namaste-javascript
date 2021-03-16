85f40817e6aaae46a06a9f75ecd105ae9d51a355
nickBrimmer <nickbrimmer@gmail.com>
2021-03-16 16:54:28 -0700

    hoisting in javascript

--- 3) Hoisting, Vars & Functions/script.js
+++ 3) Hoisting, Vars & Functions/script.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+console.log(getName); // logs function structure
+
+getName();  // calls the function!!!!! (memoization!!!)
+
+console.log(x); // Not defined
+
+var x = 7;
+
+
+getName();
+console.log(x);  // defined
+console.log(z);  // "ReferenceError: z is not defined"
+
+// logs as a traditional function.
+function getName() {
+    console.log("Namaste JavaScript");
+}
+
+// logs out like a variable.
+var getNameArrow = () => {
+    console.log("Namaste JavaScript");
+}
+
+// logs out like a variable. 
+var getName = function (){
+    console.log("Namaste JavaScript");
+}
