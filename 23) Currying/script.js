1cedb7a94e622dbb6ac903fd23e9ff0cf562808e
nickbrimmer <nickbrimmer@gmail.com>
2022-05-22 16:30:11 -0700

    Event Bubbling and Capturing

--- 23) Currying/script.js
+++ 23) Currying/script.js
@@ -1,1 +1,31 @@
-console.log('Hello World')
+let multiply = function(x, y){
+    console.log(x,y);
+    console.log(x * y);
+}
+
+// this refers to the bound function
+// first parameter will always bind 2 to x
+let multiplyByTwo = multiply.bind(this, 2)
+
+// which is the same as ...
+// let multiplyByTwo = function(x){
+//     let x = 2;
+//     console.log(x, y);
+// }
+
+multiplyByTwo(5)
+multiplyByTwo(3)
+
+
+let dragon = 
+    name => 
+        size => 
+            element => 
+                name + ' is a ' +
+                size + ' dragon that breathes ' +
+                element + '!'
+
+
+console.log(
+    dragon('fluffykins')('tiny')('lightning')
+);
--- 25) Event Bubbling, Capturing aka Trickling/script.js
+++ 25) Event Bubbling, Capturing aka Trickling/script.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+document.querySelector('#gparent')
+    .addEventListener('click', ()=> {
+        console.log('Gparent clicked!');
+    }, false);
+
+document.querySelector('#parent')
+    .addEventListener('click', (e) => {
+        console.log("Parent clicked!");
+        e.stopPropagation()
+    }, false);
+
+document.querySelector('#child')
+    .addEventListener('click', () => {
+        console.log('Child clicked!');
+    }, false);
+
