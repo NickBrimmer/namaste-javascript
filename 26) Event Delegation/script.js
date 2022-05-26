c616f7ca847483b8a1d3e79ade95dd3f61772189
nickbrimmer <nickbrimmer@gmail.com>
2022-05-26 06:28:51 -0700

    fun fun function beginnings

--- 26) Event Delegation/script.js
+++ 26) Event Delegation/script.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+document.querySelector('#categories').addEventListener('click', (e) => {
+    console.log(e.target);
+    if(e.target.tagName == "LI"){
+        window.location.href = "/"+ e.target.id
+    }
+})
+
+document.querySelector('#form').addEventListener('keyup', (e) => {
+    console.log(e);
+    if(e.target.dataset.uppercase != undefined){
+        e.target.value = e.target.value.toUpperCase();
+    }
+})
